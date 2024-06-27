3dca64d4750939d810a45125d20968596e9db75c
KawshikBss <kawshikbss@gmail.com>
2024-06-28 00:18:25 +0600

    attempt to fix build

--- app/admin/user-settings/page.tsx
+++ app/admin/user-settings/page.tsx
@@ -7,35 +7,35 @@ import ExperienceForm from "@/app/ui/pages/admin/experience-form";
 type Props = {};
 
 async function Admin({}: Props) {
-    const userRes = await fetch(
-        "http://localhost:3000/api/settings/user?id=667d280da9fed2294a2b6d00"
+    /* const userRes = await fetch(
+        "https://kawshikbss.github.io/api/settings/user?id=667d280da9fed2294a2b6d00"
     );
     const { user } = await userRes.json();
     const skillsRes = await fetch(
-        `http://localhost:3000/api/settings/skill?userId=${
+        `https://kawshikbss.github.io/api/settings/skill?userId=${
             user?._id ?? "0"
         }`
     );
     const skills = await skillsRes.json();
     const projectsRes = await fetch(
-        `http://localhost:3000/api/settings/project?userId=${
+        `https://kawshikbss.github.io/api/settings/project?userId=${
             user?._id ?? "0"
         }`
     );
     const projects = await projectsRes.json();
     const experiencesRes = await fetch(
-        `http://localhost:3000/api/settings/experience?userId=${
+        `https://kawshikbss.github.io/api/settings/experience?userId=${
             user?._id ?? "0"
         }`
     );
-    const experiences = await experiencesRes.json();
+    const experiences = await experiencesRes.json(); */
 
     return (
         <div>
-            <UserForm user={user} />
+            {/* <UserForm user={user} />
             <SkillsForm user={user} skills={skills.data} />
             <ProjectsForm user={user} projects={projects.data} />
-            <ExperienceForm user={user} experiences={experiences.data} />
+            <ExperienceForm user={user} experiences={experiences.data} /> */}
         </div>
     );
 }
