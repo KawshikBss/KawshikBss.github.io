1ad6dcdb4e66909364473a1828d5c53a89b894ba
KawshikBss <kawshikbss@gmail.com>
2024-07-04 03:01:33 +0600

    completed footer

--- app/ui/layout/footer.tsx
+++ app/ui/layout/footer.tsx
@@ -1,9 +1,46 @@
+import { ContactsData } from "@/app/lib/data";
+import Link from "next/link";
 import React from "react";
+import RenderIcon from "../components/render-icon";
 
 type Props = {};
 
 function Footer({}: Props) {
-    return <div>Footer</div>;
+    return (
+        <div className="w-full">
+            <ul className="w-full flex flex-col md:flex-row items-center">
+                {ContactsData.socialLinks.map((social: any, index: number) => (
+                    <li
+                        key={index}
+                        className="w-full md:w-1/3 px-5 py-4 text-xl font-black"
+                        style={{
+                            backgroundColor: social.color ?? "#000",
+                        }}
+                    >
+                        <Link
+                            href={social.link}
+                            target="_blank"
+                            className="flex flex-row justify-between items-center"
+                        >
+                            {social.name}
+                            <RenderIcon
+                                name={social.icon}
+                                className="w-10 h-10 text-white"
+                            />
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+            <div className="w-full py-5 bg-[#10e956] text-white">
+                <Link
+                    href={`mailto:${ContactsData.mail}`}
+                    className="text-3xl w-fit block capitalize mx-auto font-black"
+                >
+                    Shoot me a mail
+                </Link>
+            </div>
+        </div>
+    );
 }
 
 export default Footer;
