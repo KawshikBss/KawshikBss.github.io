d3bafc61f33725b56faa936229339e308d8f6f76
KawshikBss <kawshikbss@gmail.com>
2024-07-04 03:01:18 +0600

    fixed overlap

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,21 +1,17 @@
-import clsx from "clsx";
-import localFont from "next/font/local";
 import React from "react";
 import { ProjectsData } from "../lib/data";
 import { ProjectInterface } from "../lib/defenitions";
 import ProjectItem from "../ui/pages/projects/project-item";
 
-const exoSpace = localFont({ src: "../../fonts/ExoSpace.ttf" });
-
 function ProjectsShowcase() {
     return (
-        <main className={clsx("min-h-screen", exoSpace.className)}>
+        <main className="min-h-screen">
             <h1 className="text-4xl font-black text-[#14e956] mt-10 md:mt-40 mx-auto md:ml-20 w-fit">
                 {"{"}
                 <span className="text-[#FFF] mx-2">Projects Showcase</span>
                 {"}"}
             </h1>
-            <div className="flex flex-col justify-between items-center mt-12 px-20 gap-24">
+            <div className="flex flex-col justify-between items-center mt-12 px-20 pb-20 gap-24">
                 {ProjectsData.map(
                     (project: ProjectInterface, index: number) => (
                         <ProjectItem
