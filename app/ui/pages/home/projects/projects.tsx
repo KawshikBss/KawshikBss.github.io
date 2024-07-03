d741d57cfabbfbff74073a8085feb3c3a3f9fb38
KawshikBss <kawshikbss@gmail.com>
2024-07-04 01:53:17 +0600

    modified link to projects and button style

--- app/ui/pages/home/projects/projects.tsx
+++ app/ui/pages/home/projects/projects.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import Carousel from "./carousel";
 import Link from "next/link";
 import { motion } from "framer-motion";
-import { ProjectInterface } from "@/app/lib/defenitions";
 import { ProjectsData } from "@/app/lib/data";
 
 type Props = {};
@@ -31,13 +30,17 @@ function Projects({}: Props) {
                 Projects Showcase
             </motion.span>
             {ProjectsData?.length ? <Carousel items={ProjectsData} /> : ""}
-            <Link href={"/"}>
+            <Link href={"/projects"}>
                 <motion.span
                     animate={{
-                        outlineOffset: ["2px", "10px", "2px"],
+                        outlineOffset: ["2px", "16px", "2px"],
                         outlineWidth: ["0px", "4px", "0"],
                     }}
-                    transition={{ repeat: Infinity, duration: 2 }}
+                    whileHover={{
+                        outlineOffset: ["10px"],
+                        outlineWidth: ["4px"],
+                    }}
+                    transition={{ repeat: Infinity, duration: 0.6 }}
                     className="bg-[#14e956] py-3 px-8 rounded-lg text-lg text-[#12141d] font-black shadow-2xl shadow-[rgba(20,233,86,.74)] outline outline-4 outline-[#14e956] animate-pulse"
                 >
                     View All Projects
